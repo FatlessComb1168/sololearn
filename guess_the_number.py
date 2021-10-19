b5ad8b0b2d622ccc45b8f0d11f26db5d71812db8
FatlessComb1168 <84366597+FatlessComb1168@users.noreply.github.com>
2021-10-19 23:57:09 +0300

    Add files via upload

--- guess_the_number.py
+++ guess_the_number.py
@@ -31,4 +31,4 @@ while True:
         else:
             print("Unfortunately, it's the number", str(a) + '!');
     except:
-        print('An error was occured! Try again.');
+        print('An error was occurred! Try again.');
