e5e1cd28b08861893661c5542890f42d82a30898
FatlessComb1168 <84366597+FatlessComb1168@users.noreply.github.com>
2021-10-19 23:50:08 +0300

    Add files via upload

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -5,12 +5,10 @@ This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
@@ -24,24 +22,31 @@ while True:
         array = [];
 
         while True:
-            array.append(n);
+            array.append(x);
             n = x + n;
             
             if x % 2 == 0:
                 x = x / 2;
-                i = i + 1;
+                i += 1;
                 
             elif x != 1 and x % 2 != 0:
                 x = 3 * x + 1;
-                i = i + 1;
+                i += 1;
                 
             if x == 1:
+                i += 1;
+                array.append(x);
+                n = x + n;
                 break;
+
+        for i in range(len(array)):
+            array[i] = int(array[i]);
 
         n = str(int(n));
         print("\nNumber of iterations: " + str(i));
         print("Summary of all given numbers: " + n);
         print("Maximum: " + str(int(max(array))));
+        print('Numbers:', str(array)[1:-1]);
         input();
     except:
         a = input('Exit? (y/n): ');
