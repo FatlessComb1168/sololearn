b57ae5a3354f4f28b3d0bb921007c0c31d4ea0c5
Fedor Egorov <84366597+axelblatt@users.noreply.github.com>
2023-09-27 11:50:36 +0300

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,6 +1,6 @@
 '''
 Rock-paper-scissors.
-Copyright (C) 2021 Fedor Egorov <fedoregorov1@yandex.ru>
+Copyright (C) 2021 Fedor Egorov
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
